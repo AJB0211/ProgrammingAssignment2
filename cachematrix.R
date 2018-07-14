973f56fe735f7bebfbe1a1773b3db565a5c2756c
ajb0211 <ajb0211@gmail.com>
2018-07-13 20:54:53 -0400

    Did the assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,25 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+
+## This looks like a "class"
+## Quick search says this is how you do classes in R
+## this is atrocious, I hate R now
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  
+  set <- function(y){
+    x <<- y
+    inv <<- NULL
+  }
+  
+  get <- function() x
+  
+  setInv <- function(new_inv) inv <<- new_inv
+  
+  getInv <- function() inv
+  
+  list(set = set, get = get, setInverse = setInv, getInverse = getInv)
+  
 }
 
 
@@ -12,4 +27,17 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  inVal <- x$getInverse()
+  if (!is.null((inVal))) {
+    message("retrieving cached member")
+    return(inVal)
+  }
+  myMat <- x$get()
+  inVal <- solve(myMat,...)
+  x$setInverse(inVal)
+  return(inVal)
 }
+
+
+
+
